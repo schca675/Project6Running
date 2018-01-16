91b52ad987f62cc3454fb015cc5c770c4125460d
Cathy Schmit <cathy.schmit@hotmail.de>
2018-01-16 12:59:47 +0100

    Update Wisses explanation

--- wisse.m
+++ wisse.m
@@ -1,9 +1,18 @@
 %save it on git by doing:
-%Create new file
+
+%% Create new file
 %Right click on it
-%Source control
-%Add to GIT (only when making a new file)
+%-> Source control
+%----> Add to GIT (only when making a new file)
 %Click again on te file
-%Source control
-%Comit All to Git Repository --> save all your changes in the Git FOLDER
+%-> Source control
+%----> Comit All to Git Repository --> save all your changes in the local Git FOLDER
 %ENTER A COMMENT: type what you did
+
+%% Push something to the server.
+% Right click onthe "Current Folder" window
+%-> Source control
+%----> Commit All to Git Repository --> save all your changes in the local Git FOLDER
+% Do Fetch!
+%-> Source control
+%----> Push
