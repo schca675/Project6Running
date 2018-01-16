b8c097c098467d231585034f00b5191a80319c37
= <=>
2018-01-16 12:49:21 +0100

    new file wisse

---
+++ wisse.m
@@ -0,0 +1,9 @@
+%save it on git by doing:
+%Create new file
+%Right click on it
+%Source control
+%Add to GIT (only when making a new file)
+%Click again on te file
+%Source control
+%Comit All to Git Repository --> save all your changes in the Git FOLDER
+%ENTER A COMMENT: type what you did
